ac785a56c2132a7f817a75008bcb5a8abe233acb
Redadaalabi2001 <daalabireda@gmail.com>
2022-04-05 10:22:25 +0000

    adddd

--- front-end/src/components/Dashboardadmin.vue
+++ front-end/src/components/Dashboardadmin.vue
@@ -1,25 +1,36 @@
 <template>
-    <div class="container">
+    <!-- <div class="container"> -->
+    <center><h1 class="my-5">Welcom to dashboard</h1></center>
     <div cl ass="row my-5">
         <div class="col-md-12 mx-auto">
             <div class="card mt-5">
-                <div class="p-3 mb-2 bg-light text-dark">
+                <div class="p-3 mb-2 text-dark">
                     <table class="table table-hover">
-                        <thead>
+                        <thead class="back">
                             <tr>
-                            <th scope="col">Nom</th>
+                            <th scope="col">id</th>
+                            <th scope="col">nom</th>
                             <th scope="col">Prenom</th>
                             <th scope="col">Age</th>
                             <th scope="col">Profession</th>
+                            <th scope="col">reference_client</th>
+                            <th scope="col">sujet</th>
+                            <th scope="col">date</th>
+                            <th scope="col">creneau</th>
                             <th scope="col">Action</th>
                             </tr>
                         </thead>
-                        <tbody> 
+                        <tbody v-for="client in clients" :key="client.id"> 
                             <tr>
-                                <th scope="row" class="font-weight-normal">DAALABI</th>
-                                <th scope="row" class="font-weight-normal">Reda</th>
-                                <th scope="row" class="font-weight-normal">20</th>
-                                <th scope="row" class="font-weight-normal">Developpeur</th>
+                                <th scope="row" class="font-weight-normal">{{ client.id }}</th>
+                                <th scope="row" class="font-weight-normal">{{ client.nom }}</th>
+                                <th scope="row" class="font-weight-normal">{{ client.prenom }}</th>
+                                <th scope="row" class="font-weight-normal">{{ client.age }}</th>
+                                <th scope="row" class="font-weight-normal">{{ client.profession }}</th>
+                                <th scope="row" class="font-weight-normal">{{ client.profession }}</th>
+                                <th scope="row" class="font-weight-normal">{{ client.sujet }}</th>
+                                <th scope="row" class="font-weight-normal">{{ client.date }}</th>
+                                <th scope="row" class="font-weight-normal">{{ client.creneau }}</th>
                                 <td class="d-flex flex-row justify-content-center">
                                     <form method="" action="" class="mx-1">
                                         <input type="hidden" name="id" value="">
@@ -31,30 +42,55 @@
                                     </form>
                                 </td>
                             </tr>
-                            <tr>
-                                <th scope="row" class="font-weight-normal">Belkhoukh</th>
-                                <th scope="row" class="font-weight-normal">Abdo</th>
-                                <th scope="row" class="font-weight-normal">27</th>
-                                <th scope="row" class="font-weight-normal">Developpeur</th>
-                            </tr>
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
     </div>
-</div>
+<!-- </div> -->
 <br>
 <br>
 <br>
 </template>
 
 <script>
+
     export default {
-        name: "Dashboardadmin"
+        name: "Dashboardadmin",
+        data() {
+            return {
+                clients: [],
+                client: {
+                    id: '',
+                    nom: '',
+                    prenom: '',
+                    age: '',
+                    profession: '',
+                    reference_client: '',
+                    sujet: '',
+                    date: '',
+                    creneau: ''
+                }
+                
+            };
+        },
+        created(){
+            this.getAllclient();
+        },
+        methods : {
+            getAllclient(){
+                axios.get('http://localhost/gestion-rndv/back-end/controllers/C-lire_rdv.php')
+                    .then(response => this.clients = response.data)
+                    .catch(err => console.log(err));
+            },
+        }
     }
 </script>
 
 <style lang="scss" scoped>
+.back {
+    background-color: #B1D0E0;
+}
 
 </style>
